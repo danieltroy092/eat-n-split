692bb24a3141e5add16fb54766c6401047f795c9
Troy Ong <troy.ong@hoffi.com>
2026-05-20 15:48:16 +0100

    added form split-bill +  finished building components and layout

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -74,13 +75,38 @@ function Button({ children }) {
 function FormAddFriend() {
   return (
     <form className="form-add-friend">
-      <label>Friend name</label>
+      <label>🧑‍🤝‍🧑 Friend name</label>
       <input type="text" />
 
-      <label>Image URL</label>
+      <label>📸 Image URL</label>
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with x</h2>
+
+      <label>💸 Bill value</label>
+      <input type="text" />
+
+      <label>👦 Your expense</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
     </form>
   );
 }
